3050003edea2dd3b7d7c73d5d963b959dcd2b7a4
Angel Santana <angelsilvadesantana@gmail.com>
2026-06-01 00:49:12 +0000

    seeing how the vercel bot works

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home(){
-    return  <h1>Hello, world!</h1>
-    
+    return  <><h1>Hello, world!</h1>
+    <p>That's my deploy training to get comfortable with vercel!</p>
+    </>
 }
 
 export default Home
