bec4d9a3fedcce05d5d89af9fcbebe2b7780931c
Angel Santana <angelsilvadesantana@gmail.com>
2026-06-21 00:07:55 +0000

    adicionado `editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-function Home(){
-    return  <><h1>Hello, world!</h1>
-    <p>That's my deploy training to get comfortable with vercel!</p>
+function Home() {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <p>That's my deploy training to get comfortable with vercel!</p>
     </>
+  );
 }
 
-export default Home
+export default Home;
